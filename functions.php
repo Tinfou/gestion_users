e4842eab0d709e2416dad548bda20a297f7d4f1e
Tinfou <mahery.rak@outlook.fr>
2024-05-22 15:17:21 +0300

    ajout fonction is_username_valid

--- functions.php
+++ functions.php
@@ -10,3 +10,11 @@ function get_status($status): void
         echo "inactif";
     }
 }
+//Entre 3 et 20 caractères/Peut contenir des lettres majuscules et minuscules/Peut contenir des chiffres/Peut contenir des caractères spéciaux limités, souvent '_' et '-'
+function is_username_valid($username) : bool{
+    if(preg_match('/^[A-Za-z0-9_-]{3,20}$/',$username)){
+        return true;
+    }else{
+        return false;
+    }
+}
